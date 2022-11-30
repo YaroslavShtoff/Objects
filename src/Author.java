e58ec815d55ae938dd6b41fe1e35bee04f730334
yarik <yarikshtoda1988@gmail.com>
2022-11-30 23:23:00 +0300

    Две темы в одном файле

---
+++ src/Author.java
@@ -0,0 +1,42 @@
+import java.util.Objects;
+
+public class Author {
+    private String name;
+    private String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Author author = (Author) o;
+        return getName().equals(author.getName()) && author.getSurname().equals(author.getSurname());
+    }
+
+    @Override
+    public int hashCode () {
+        return Objects.hash(name, surname);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Author pushkin = new Author("Александр", " Пушкин ");
+        Author turgenev = new Author("Иван", " Тургенев ");
+
+        Book goldenFish = new Book("Золотая рыбка", pushkin, 1835);
+        Book fathersAndSons = new Book("Отци и дети ", turgenev, 1867);
+
+        System.out.println(goldenFish.getTitle() +" "+ goldenFish.getAuthorSurname() + " " + goldenFish.getAuthorName()
+                +" "+goldenFish.getYearPublishing() );
+        System.out.println(fathersAndSons.getTitle() +" "+ fathersAndSons.getAuthorSurname() + " "
+                + fathersAndSons.getAuthorName()
+                +" "+fathersAndSons.getYearPublishing() );
+        fathersAndSons.setYearPublishing(1865);
+        System.out.println(fathersAndSons.getTitle() +" "+ fathersAndSons.getAuthorSurname() + " "
+                + fathersAndSons.getAuthorName()
+                +" "+fathersAndSons.getYearPublishing() );
+
+        System.out.println(pushkin.toString());
+        System.out.println(goldenFish.toString());
+
+
+    }
+}
